d3d410762bd02ced3eb5dbf542b27f58b58fb38e
kevinjs <kevinjonessaleh@icloud.com>
2022-11-19 23:54:52 +0000

    Modified battery.py to account for max power
    Modified batteryModule.py to add getters
    
    Modified bms.py changing the demandPower, loadBalance, powerOn and powerOff methods

--- batteryModule.py
+++ batteryModule.py
@@ -5,7 +5,24 @@ from  voltageSensor import VoltageSensor
 
 class BatteryModule(object):
     def __init__(self, max_voltage, max_current):
-        self.batteryCell = BatteryCell(max_voltage, max_current)
-        self.currentSensor = CurrentSensor()
-        self.voltageSensor = VoltageSensor()
-        self.temperatureSensor = TemperatureSensor()
+        self._batteryCell = BatteryCell(max_voltage, max_current)
+        self._currentSensor = CurrentSensor()
+        self._voltageSensor = VoltageSensor()
+        self._temperatureSensor = TemperatureSensor()
+        
+    def getBatteryCell(self):
+        return self._batteryCell
+
+    def getCurrentSensor(self):
+        return self._currentSensor
+
+    def getVoltageSensor(self):
+        return self._voltageSensor
+
+    def getTemperatureSensor(self):
+        return self._temperatureSensor
+
+    batteryCell = property(getBatteryCell)
+    currentSensor = property(getCurrentSensor)
+    voltageSensor = property(getVoltageSensor)
+    temperatureSensor = property(getTemperatureSensor)
